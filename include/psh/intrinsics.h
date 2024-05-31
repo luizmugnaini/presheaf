c98951fc3e08635fc0f3420f3f985b2ca57ebf31
Luiz G. Mugnaini A. <luizmugnaini@gmail.com>
2024-05-31 20:09:04 -0300

    Remove meaningless compiler flags and add -Wformat=2
    Signed-off-by: Luiz G. Mugnaini A. <luizmugnaini@gmail.com>

--- include/psh/intrinsics.h
+++ include/psh/intrinsics.h
@@ -46,7 +46,7 @@
 #define psh_min(lhs, rhs) (((lhs) < (rhs)) ? (lhs) : (rhs))
 #define psh_max(lhs, rhs) (((lhs) > (rhs)) ? (lhs) : (rhs))
 
-#define psh_is_pow_of_two(n) (((n) > 0) && !((n) & ((n)-1)))
+#define psh_is_pow_of_two(n) (((n) > 0) && !((n) & ((n) - 1)))
 
 #define psh_clamp(x, min, max) (((x) < (min)) ? (min) : (((x) > (max)) ? (max) : (x)))
 
