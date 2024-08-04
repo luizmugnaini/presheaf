6dded7124bf65ff17ad5328aded84fc5ba378516
Luiz Mugnaini <luizmugnaini@gmail.com>
2024-08-03 23:31:32 -0300

    Add arena methods that allow manually aligned memory allocations
    Signed-off-by: Luiz Mugnaini <luizmugnaini@gmail.com>

--- include/psh/arena.h
+++ include/psh/arena.h
@@ -139,7 +139,7 @@ namespace psh {
     ///     * Any functions that return a raw pointer should be checked for nullity since allocation
     ///       may fail.
     struct Arena {
-        u8*   buf    = nullptr;  ///< Not-owned memory block managed by the arena allocator.
+        u8*   buf    = nullptr;  ///< Non-owned memory block managed by the arena allocator.
         usize size   = 0;        ///< The capacity, in bytes, of the memory block.
         usize offset = 0;        ///< The current offset to the free-space in the memory block.
 
@@ -147,34 +147,20 @@ namespace psh {
         // - Allocation methods -
         // -----------------------------------------------------------------------------
 
-        /// Allocates a block of memory that fits a given number of entities of type `T`.
-        ///
-        /// Parameters:
-        ///     * count: Number of entities of type `T` that should fit in the new block.
+        u8* alloc_align(usize size, u32 alignment) noexcept;
+        u8* zero_alloc_align(usize size, u32 alignment) noexcept;
+        u8* realloc_align(
+            u8*   block,
+            usize current_size_bytes,
+            usize new_size_bytes,
+            u32   alignment) noexcept;
+
         template <typename T>
         T* alloc(usize count) noexcept;
 
-        /// Allocate a zeroed block of memory.
-        ///
-        /// Allocates a block of memory of a given size for a given type, accounting for the
-        /// alignment needed for the given type `T`. Additionally the block is zeroed before
-        /// returning to the caller.
-        ///
-        /// Parameters:
-        ///     * count: Number of entities of type `T` that should fit in the new block.
         template <typename T>
         T* zero_alloc(usize count) noexcept;
 
-        /// Reallocate a block of memory of a given type.
-        ///
-        /// The memory alignment required for the type `T` is automatically handled for the user.
-        ///
-        /// Parameters:
-        ///     * block: Pointer to the start of the memory block to be resized.
-        ///     * current_count: Current number of entities of type `T` that `block` is capable
-        ///       of holding.
-        ///     * new_count: Number of entities of type `T` that the new memory block should be
-        ///       able to contain.
         template <typename T>
         T* realloc(T* block, usize current_count, usize new_count) noexcept;
 
@@ -195,101 +181,20 @@ namespace psh {
 
     template <typename T>
     T* Arena::alloc(usize count) noexcept {
-        if (psh_unlikely(count == 0 || size == 0)) {
-            return nullptr;
-        }
-
-        uptr  memory_addr    = reinterpret_cast<uptr>(buf);
-        uptr  new_block_addr = align_forward(memory_addr + offset, alignof(T));
-        usize block_size     = sizeof(T) * count;
-
-        // Check if there is enough memory.
-        if (psh_unlikely(new_block_addr + block_size > size + memory_addr)) {
-            psh_error_fmt(
-                "ArenaAlloc::alloc unable to allocate %zu bytes of memory (%zu bytes required "
-                "due to alignment). The allocator has only %zu bytes remaining.",
-                block_size,
-                wrap_sub(size + new_block_addr, (offset + memory_addr)),
-                block_size - offset);
-            return nullptr;
-        }
-
-        // Commit the new block of memory.
-        offset = static_cast<usize>(block_size + new_block_addr - memory_addr);
-
-        return reinterpret_cast<T*>(new_block_addr);
+        return reinterpret_cast<T*>(this->alloc_align(sizeof(T) * count, alignof(T)));
     }
 
     template <typename T>
     T* Arena::zero_alloc(usize count) noexcept {
-        T* const ptr = alloc<T>(count);
-        if (ptr != nullptr) {
-            std::memset(ptr, 0, count);
-        }
-        return ptr;
+        return reinterpret_cast<T*>(this->zero_alloc_align(sizeof(T) * count, alignof(T)));
     }
 
     template <typename T>
     T* Arena::realloc(T* block, usize current_count, usize new_count) noexcept {
-        // Check if there is any memory at all.
-        if (psh_unlikely(size == 0 || buf == nullptr || new_count == 0)) {
-            return nullptr;
-        }
-
-        // Check if the user wants to allocate a completely new block.
-        if (psh_unlikely(block == nullptr || current_count == 0)) {
-            return alloc<T>(new_count);
-        }
-
-        u8* block_bytes = reinterpret_cast<u8*>(block);
-        u8* mem_end     = buf + size;
-        u8* free_mem    = buf + offset;
-
-        // Check if the block lies within the allocator's memory.
-        if (psh_unlikely((block_bytes < buf) || (block_bytes >= buf + size))) {
-            psh_error("ArenaAlloc::realloc called with pointer outside of its domain.");
-            return nullptr;
-        }
-
-        // Check if the block is already free.
-        if (psh_unlikely(block_bytes >= free_mem)) {
-            psh_error("ArenaAlloc::realloc called with a pointer to a free address of the arena "
-                      "domain.");
-            return nullptr;
-        }
-
-        usize current_block_size = sizeof(T) * current_count;
-        usize new_block_size     = sizeof(T) * new_count;
-
-        psh_assert_msg(
-            current_block_size <= offset,
-            "Arena::realloc called with current_block_size surpassing the current offset of the "
-            "arena, which isn't possible");
-
-        // If the block is the last allocated, just bump the offset.
-        if (block_bytes == free_mem - current_block_size) {
-            // Check if there is enough space.
-            if (psh_unlikely(block_bytes + new_block_size > mem_end)) {
-                psh_error_fmt(
-                    "ArenaAlloc::realloc unable to reallocate block from %zu bytes to %zu "
-                    "bytes.",
-                    current_block_size,
-                    new_block_size);
-                return nullptr;
-            }
-
-            offset = static_cast<usize>(
-                static_cast<isize>(offset) +
-                static_cast<isize>(new_block_size - current_block_size));
-            return block;
-        }
-
-        u8* new_block = zero_alloc<u8>(new_count);
-
-        // Copy the existing data to the new block.
-        usize copy_size = psh_min(current_block_size, new_block_size);
-        memory_copy(new_block, block_bytes, copy_size);
-
-        return reinterpret_cast<T*>(new_block);
+        return reinterpret_cast<T*>(this->realloc_align(
+            reinterpret_cast<u8*>(block),
+            sizeof(T) * current_count,
+            sizeof(T) * new_count,
+            alignof(T)));
     }
 }  // namespace psh
--- src/arena.cc
+++ src/arena.cc
@@ -25,8 +25,108 @@ namespace psh {
     // - Arena implementation -
     // -----------------------------------------------------------------------------
 
+    u8* Arena::alloc_align(usize size_bytes, u32 alignment) noexcept {
+        if (psh_unlikely(size_bytes == 0 || this->size == 0)) {
+            return nullptr;
+        }
+
+        uptr  memory_addr    = reinterpret_cast<uptr>(this->buf);
+        uptr  new_block_addr = align_forward(memory_addr + this->offset, alignment);
+        usize block_size     = sizeof(u8) * size_bytes;
+
+        // Check if there is enough memory.
+        if (psh_unlikely(new_block_addr + block_size > size + memory_addr)) {
+            psh_error_fmt(
+                "ArenaAlloc::alloc unable to allocate %zu bytes of memory (%zu bytes required "
+                "due to alignment). The allocator has only %zu bytes remaining.",
+                block_size,
+                wrap_sub(this->size + new_block_addr, (this->offset + memory_addr)),
+                block_size - this->offset);
+            return nullptr;
+        }
+
+        // Commit the new block of memory.
+        this->offset = static_cast<usize>(block_size + new_block_addr - memory_addr);
+
+        return reinterpret_cast<u8*>(new_block_addr);
+    }
+
+    u8* Arena::zero_alloc_align(usize size_bytes, u32 alignment) noexcept {
+        u8* const ptr = this->alloc_align(size_bytes, alignment);
+        psh::memory_set(psh::FatPtr{ptr, size_bytes}, 0);
+        return ptr;
+    }
+
+    u8* Arena::realloc_align(
+        u8*   block,
+        usize current_size_bytes,
+        usize new_size_bytes,
+        u32   alignment) noexcept {
+        // Check if there is any memory at all.
+        if (psh_unlikely((this->size == 0) || (this->buf == nullptr) || (new_size_bytes == 0))) {
+            return nullptr;
+        }
+
+        // Check if the user wants to allocate a completely new block.
+        if (psh_unlikely(block == nullptr) || (current_size_bytes == 0)) {
+            return this->alloc_align(new_size_bytes, alignment);
+        }
+
+        u8* block_bytes = reinterpret_cast<u8*>(block);
+        u8* mem_end     = this->buf + this->size;
+        u8* free_mem    = this->buf + this->offset;
+
+        // Check if the block lies within the allocator's memory.
+        if (psh_unlikely((block_bytes < this->buf) || (block_bytes >= this->buf + this->size))) {
+            psh_error("ArenaAlloc::realloc called with pointer outside of its domain.");
+            return nullptr;
+        }
+
+        // Check if the block is already free.
+        if (psh_unlikely(block_bytes >= free_mem)) {
+            psh_error("ArenaAlloc::realloc called with a pointer to a free address of the arena "
+                      "domain.");
+            return nullptr;
+        }
+
+        usize current_block_size = sizeof(u8) * current_size_bytes;
+        usize new_block_size     = sizeof(u8) * new_size_bytes;
+
+        psh_assert_msg(
+            current_block_size <= this->offset,
+            "Arena::realloc called with current_block_size surpassing the current offset of the "
+            "arena, which isn't possible");
+
+        // If the block is the last allocated, just bump the offset.
+        if (block_bytes == free_mem - current_block_size) {
+            // Check if there is enough space.
+            if (psh_unlikely(block_bytes + new_block_size > mem_end)) {
+                psh_error_fmt(
+                    "ArenaAlloc::realloc unable to reallocate block from %zu bytes to %zu "
+                    "bytes.",
+                    current_block_size,
+                    new_block_size);
+                return nullptr;
+            }
+
+            this->offset = static_cast<usize>(
+                static_cast<isize>(this->offset) +
+                static_cast<isize>(new_block_size - current_block_size));
+            return block;
+        }
+
+        u8* new_block = this->zero_alloc_align(new_size_bytes, alignment);
+
+        // Copy the existing data to the new block.
+        usize copy_size = psh_min(current_block_size, new_block_size);
+        memory_copy(new_block, block_bytes, copy_size);
+
+        return new_block;
+    }
+
     void Arena::clear() noexcept {
-        offset = 0;
+        this->offset = 0;
+        int x        = 0;
     }
 
     ScratchArena Arena::make_scratch() noexcept {
@@ -37,14 +137,20 @@ namespace psh {
     // - Scratch arena implementation -
     // -----------------------------------------------------------------------------
 
-    ScratchArena::ScratchArena(Arena* parent) noexcept
-        : arena{parent}, saved_offset{parent->offset} {}
+    ScratchArena::ScratchArena(Arena* parent) noexcept {
+        if (parent != nullptr) {
+            this->arena        = parent;
+            this->saved_offset = parent->offset;
+        }
+    }
 
     ScratchArena::~ScratchArena() noexcept {
-        arena->offset = saved_offset;
+        if (this->arena != nullptr) {
+            this->arena->offset = this->saved_offset;
+        }
     }
 
     ScratchArena ScratchArena::decouple() const noexcept {
-        return ScratchArena{arena};
+        return ScratchArena{this->arena};
     }
 }  // namespace psh
